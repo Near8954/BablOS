2816ac4ca37e8f8a62545e3fd33d5d83e535d272
Roman Baldin <roma.baldin@gmail.com>
2026-09-17 18:57:05 +0700

    solution

--- Makefile
+++ Makefile
@@ -4,18 +4,19 @@
 # Build tools
 NASM = nasm -f bin 
 
-
+N?=421633
 # =============================================================================
 # Tasks
 
 all: clean build test
 
 .tmp/boot.bin: src/boot.asm
-	$(NASM) src/boot.asm -o .tmp/boot.bin
+	$(NASM) src/boot.asm -o .tmp/boot.bin -dN=$(N)
 
 boot.img: .tmp/boot.bin
 	dd if=/dev/zero of=boot.img bs=1024 count=1440
 	dd if=.tmp/boot.bin of=boot.img conv=notrunc
+	dd if=foo.txt of=boot.img conv=notrunc seek=1
 
 build: boot.img
 
--- src/boot.asm
+++ src/boot.asm
@@ -1,4 +1,66 @@
 [BITS 16]
+[ORG 0x7C00]
+
+%assign SECTORS (N + 511) / 512
+
+cli
+xor ax, ax
+mov ss, ax
+mov sp, 0x7C00
+mov ds, ax
+sti
+
+mov si, SECTORS
+
+ xor ax, ax
+mov es, ax
+mov bx, 0x7E00
+
+mov ch, 0 ; cylinder
+mov dh, 0 ; head
+mov cl, 1 ; sector
+
+.loop:
+  cmp cl, 18
+  jge .change_head ; main part of cycle
+  inc cl
+
+  mov ah, 0x2
+  mov al, 1
+  mov bx, 0x7E00
+
+  int 0x13
+  jc .disk_error
+  ; offset
+  mov ax, es
+  add ax, 0x0020 
+  mov es, ax
+
+
+  dec si
+  jz loop
+  jmp .loop
+
+.change_cylinder:
+  inc ch
+  mov dh, 0
+  mov cl, 0
+  jmp .loop
+
+
+.change_head:
+  cmp dh, 1
+  jge .change_cylinder
+  inc dh
+  mov cl, 0
+  jmp .loop
+
+
+.disk_error:
+  mov ah, 0x0E
+  mov al, 'E'
+  int 0x10
+  jmp .disk_error
 
 loop:
   jmp loop
